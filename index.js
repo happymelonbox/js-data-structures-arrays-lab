e14339d03752693c7fc09c6c16246c731721a9df
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-12 13:29:10 +1000

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+    drivers.push(name)
+}
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(name){
+    drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(name){
+    drivers.shift()
+}
+
+function appendDriver(name){
+    const newDriver = [...drivers, name]
+    return newDriver
+}
+
+function prependDriver(name){
+    const newDriverStart = [name,...drivers]
+    return newDriverStart
+}
+
+function removeLastDriver(){
+    const newDriverLastRemove = drivers.slice(0,drivers.length-1)
+    return newDriverLastRemove
+}
+
+function removeFirstDriver(){
+    const newDriverFirstRemove = drivers.slice(1)
+    return newDriverFirstRemove
+}
